7599fd748dea770ccb60daf6d502b9e2a2bac129
manueljm2001 <coymegaman100@gmail.com>
2024-12-16 19:59:40 +0100

    Add files via upload

---
+++ ScriptBasicoManuel.ps1
@@ -0,0 +1,15 @@
+﻿# Script básico en PowerShell.
+
+# Instrucción 1: Mostrar un mensaje en la consola.
+Write-Host "Hola, este es un script básico de PowerShell."
+
+# Instrucción 2: Obtener información sobre el sistema.
+Get-ComputerInfo
+
+# Instrucción 3: Crear un archivo de texto en el escritorio.
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$filename = "$desktopPath\ejemplo.txt"
+"Este es un archivo de ejemplo creado por PowerShell." | Out-File -FilePath $filename
+
+# Mensaje final del script.
+Write-Host "El archivo de ejemplo.txt ha sido creado en el escritorio."
